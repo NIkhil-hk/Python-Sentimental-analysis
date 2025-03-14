b74f4592a3724816b84158491587c49c45e6fee0
Nikhil-hk <nikhilbeldar333@gmail.com>
2025-03-14 08:12:20 +0530

    code

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://127.0.0.1:8000/analyze"
+url = "https://python-sentimental-analysis.onrender.com/analyze"
 data = {"text": "This is an bad project!"}
 response = requests.post(url, json=data)
 print(response.json())
